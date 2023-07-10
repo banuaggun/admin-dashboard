c1ca5a5aceecfc2a3dc7d2f52ef56aef186bfefc
banuaggun <banuaggun@outlook.com>
2023-07-10 13:37:19 +0300

    Creating the route structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './scss/App.scss'
 import Blank from './pages/Blank'
+import Layout from './layout/Layout'
+import Dashboard from './pages/Dashboard'
 
 function App() {
 
   return (
-    <>
-     <Blank/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Dashboard/>}/>
+          <Route path="customers" element={<Blank/>}/>
+          <Route path="products" element={<Blank/>}/>
+          <Route path="stats" element={<Blank/>}/>
+          <Route oath="settings" element={<Blank/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/layout/Layout.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './layout.scss'
+import {Outlet} from 'react-router-dom'
+import Sidebar from '../components/sidebar/Sidebar'
+import Topnav from '../components/topnav/Topnav'
+
+const Layout = () => {
+  return (
+    <>
+      <Sidebar/>
+      <div className='main'>
+        <div className="main__content">
+          <Topnav/>
+          <Outlet/>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Layout
