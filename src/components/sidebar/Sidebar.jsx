ded63da760ab12fdce4d90b94dacccdfedce5c20
banuaggun <banuaggun@outlook.com>
2023-07-12 14:48:43 +0300

    Icon library added to suit 2 different uses

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { useState } from 'react'
+import './sidebar.scss'
+//import {images} from '../../constants/images.js'
+import logo from '../../assets/images/logo.jpg'
+import sidebarNav from '../../configs/sidebarNav'
+import { Heart, Cube, Circle } from 'phosphor-react'
+
+
+
 
 const Sidebar = () => {
+  const [activeIndex, setActiveIndex] = useState(0)
   return (
-    <div>Sidebar</div>
+    <div className="sidebar">
+      <div className='sidebar__logo'>
+        <img src={logo} alt="logo" style={{width:'120px', height:'120px'}}/>
+        <br/><br/><br/>
+        <Heart color="#AE2983" weight="fill" size={32} />
+      <Cube color="teal" weight="duotone" />
+      <Circle color="gray" weight="thin" size={28}/>
+      <i className="ph-thin ph-airplane"></i>
+        <div className='sidebar__close'>
+          <i className='ph ph-x-circle'></i>
+          sidebar
+        </div>
+      </div>
+    </div>
   )
 }
 
